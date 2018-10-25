bcfe5d4fc7e247048945d8f61b0c0af38f2f8d78
Rich Nakasato <richnakasato@hotmail.com>
2018-10-24 21:27:36 -0400

    testing integer and double division (both ways) and unsigned minus underflow

---
+++ math_conversions/math_conversions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    double d = 2.5;
+    int i = 2;
+
+    std::cout << "d/i=" << d/i << std::endl;
+    std::cout << "i/d=" << i/d << std::endl;
+    std::cout << "25u-50=" << 25u - 50 << std::endl;
+
+    return 0;
+}
