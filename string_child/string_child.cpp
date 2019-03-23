23bd2dadbee5a01c391ea0c6f500c107fb585a0e
Rich Nakasato <richnakasato@hotmail.com>
2019-03-22 21:03:14 -0400

    tried creating reproducer

--- string_child/string_child.cpp
+++ string_child/string_child.cpp
@@ -1,4 +1,4 @@
-#define DO_OP
+//#define DO_OP
 
 #include <iostream>
 #include <string>
@@ -6,6 +6,71 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
+struct Foo
+{
+    std::string str;
+    char* buf;
+    size_t sz;
+
+    Foo()
+    {
+        std::cout << "ctor" << std::endl;
+        str = "";
+        buf = nullptr;
+        sz = 0;
+    }
+
+    Foo(const char* s)
+    {
+        std::cout << "cp ctor" << std::endl;
+        str = std::string(s);
+        buf = nullptr;
+        sz = 0;
+    }
+
+    ~Foo()
+    {
+        std::cout << "dtor" << std::endl;
+        if (sz)
+            delete[] buf;
+        sz = 0;
+    }
+
+#ifdef DO_OP
+    Foo& operator=(const char* s)
+    {
+        std::cout << "op=" << std::endl;
+        str = std::string(s);
+        return *this;
+    }
+#endif //DO_OP
+
+    void copy_assign(const char* s)
+    {
+        Foo other(s);
+        str = other.str;
+        buf = other.buf;
+        sz = other.sz;
+    }
+
+    char* bar(void)
+    {
+        size_t new_sz = str.size()+1;
+        if (sz < new_sz) {
+            delete[] buf;
+            buf = new char[new_sz];
+            sz = new_sz;
+        }
+        for (size_t i=0; i<sz-1; ++i) {
+            buf[i] = str[i];
+        }
+        buf[sz-1] = 0;
+        return buf;
+    }
+};
+
 
 struct Thing
 {
@@ -32,6 +97,7 @@ struct Thing
         std::cout << "dtor" << std::endl;
     }
 };
+
 
 class String : public std::string
 {
@@ -87,7 +153,6 @@ public:
 
 int main()
 {
-    /*
     const char* ccp;
     String a;
     a = "short";
@@ -95,10 +160,20 @@ int main()
     a = "this string should be long";
     ccp = a.foo();
     std::cout << "--\n" << ccp << std::endl;
-    */
 
+    /*
     Thing a;
     a = 1;
+    */
+
+    Foo foo;
+    foo = "test";
+    const char* foobar = foo.bar();
+    std::cout << "--\n" << foobar << std::endl;
+    //foo = "another test";
+    foo.copy_assign("another test");
+    foobar = foo.bar();
+    std::cout << "--\n" << foobar << std::endl;
 
     return 0;
 }
